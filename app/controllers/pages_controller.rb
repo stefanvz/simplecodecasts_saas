60e8a50cf8cdf694d141ddf4d5d48b34b2918004
Stefan vZ <stefanvz@me.com>
2016-01-14 20:25:16 +0000

    added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def home
+        @basic_plan = Plan.find(1) #chain with .name and so on
+        @pro_plan = Plan.find(3)
     end
     
     def about
